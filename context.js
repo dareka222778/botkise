a0d2f7d5fb25a926deb4eb59d92a67b55588a0ba
dareka222778 <dareka222778@gmail.com>
2026-02-05 14:12:59 -0300

    Update context.js

--- context.js
+++ context.js
@@ -1,1 +1,13 @@
+import { DEFAULTS } from "./config.js";
+
+export function makeContext({ client, storage, embeds, cooldown, openrouter }) {
+  return {
+    client,
+    storage,
+    embeds,
+    cooldown,
+    openrouter,
+    defaults: DEFAULTS
+  };
+}
 
