cd5a13318f6b4eea0bdf54fd37ab23851acf7d3c
dareka222778 <dareka222778@gmail.com>
2026-02-04 22:25:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online como ${client.user.tag}`);
+});
+
+client.on("messageCreate", msg => {
+  if (msg.author.bot) return;
+
+  if (msg.mentions.has(client.user)) {
+    msg.reply("Estou observando tudo 👁️");
+  }
+});
+
+client.login(process.env.MTQ2ODc2MjYwMjU2MDgxNTIwNA.GBuwsQ.Z9-NQJMCzcAqoNNpokvpVt2OGx-D9zc1BweIiQ);
